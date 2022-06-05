76e2feeda840cc68c244a35b6410cd985b6df748
CatarineRodrigues <catarine_pacheco@hotmail.com>
2022-06-05 18:05:03 -0300

    Adicionar arquivos no repositorio local

---
+++ Pizzaria/app/src/main/java/br/com/zup/pizzaria/ProdutoFragment/ProdutoFragment.kt
@@ -0,0 +1,17 @@
+package br.com.zup.pizzaria.ProdutoFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.com.zup.pizzaria.R
+
+open class ProdutoFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_produto, container, false)
+    }
+}
